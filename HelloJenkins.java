ad8db166cdda521b10bd9016d869662035d92bc6
RASHMI JADHAV <66132594+rrjadhav2000@users.noreply.github.com>
2022-08-02 16:37:20 +0530

    Create HelloJenkins.java

---
+++ HelloJenkins.java
@@ -0,0 +1,8 @@
+public class HelloJenkins {
+    public static void main(String[] s) {
+        System.out.println("Hello Oracle");
+        for(int i = 1; i<=10; i++) {
+            System.out.println("Value of K : " + i);
+        }
+    }
+}
